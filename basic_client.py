b537c0fa729564c363c4d180edae6457fc6c6306
Hugo Tamaki <hugokt2005@yahoo.com.br>
2018-08-12 16:20:57 -0300

    Changing files from place

--- basic_client.py
+++
@@ -1,7 +0,0 @@
-from client import *
-
-class BasicClient(Client):
-  def transfer(self, client, value):
-    if self.balance > value + 10:
-      self.balance = self.balance - (value + 10)
-      client.deposit(value)
